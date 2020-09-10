148d210429f18fdcbf0a2f285d3fbc28625f9e0b
Advaith Bhat <advaithbhat9@gmail.com>
2020-09-10 02:16:29 +0000

    RMX1921: Compile with Proton Clang
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,8 @@ TARGET_KERNEL_SOURCE := kernel/realme/RMX1921
 TARGET_KERNEL_CONFIG := RMX1921_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
+KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm710
